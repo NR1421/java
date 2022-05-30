d9db63db622957ff55d6421b4e057e9651499a72
NR1421 <106266771+NR1421@users.noreply.github.com>
2022-05-30 14:22:40 +0530

    Add files via upload

---
+++ Hands-on/Arithmetic/ArithmeticManual.java
@@ -0,0 +1,28 @@
+package corejava;
+// java program for arithmetic value
+public class ArithmeticManual {
+public static void main(String[] args) {
+	
+	  // Declaration
+		int a, b, add, sub, mul, div, mod;
+		
+		// Initialization
+		a = 5 ; 
+	    b = 2 ; 
+	    
+	    add = a + b;
+	    sub = a - b;
+	    mul = a * b;
+	    div = a / b;
+	    mod = a % b;
+	    
+	 // Printing Results
+	    System.out.println("Addition of two numbers = " + add); 
+	    System.out.println("Subtraction of two numbers = " + sub);
+	    System.out.println("Multiplication of two numbers = " + mul);
+	    System.out.println("Division of two numbers = " + div);
+	    System.out.println("Modulus of two numbers = " + mod);
+	}
+
+}
+
